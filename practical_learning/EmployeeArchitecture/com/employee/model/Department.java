3b5ce07df168f5802eff12b55e356b4b09b3255a
Devansh Seth <129330424+Devansh-Seth-DEV@users.noreply.github.com>
2026-03-01 00:16:28 +0530

    Add Department class with properties and methods

---
+++ practical_learning/EmployeeArchitecture/com/employee/model/Department.java
@@ -0,0 +1,51 @@
+package com.employee.model;
+
+public class Department {
+    private int departmentId;
+    private String departmentName;
+    private int managerId;
+
+    public Department(
+    	int departmentId,
+    	String departmentName,
+        int managerId
+    ) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.managerId = managerId;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+    
+    @Override
+    public String toString() {
+    	return "department_id: %s, department_name: '%s', manager_id: %s"
+    		   .formatted(
+				   departmentId,
+				   departmentName,
+				   managerId
+    		   );
+    }
+}
